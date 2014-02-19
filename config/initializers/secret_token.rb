d0510cf4d8cf1f813e3e4e2bd2c0ec3804f106dd
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-18 21:23:36 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeskApp::Application.config.secret_token = 'e39331efc0a9ace8ffe4fd6c7b36dd62f8078caaf7025100757a6d8c426169a8e82402a465a22601953f39198c5b76eaaf406148f4fef68b26186ce2eaa9b9d8'
