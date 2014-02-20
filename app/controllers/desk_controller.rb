704cfc7894c69897d7c0f4a01776d7cf01907d67
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-19 21:18:02 -0800

    Added spec controllers and modified desk reponse

--- app/controllers/desk_controller.rb
+++ app/controllers/desk_controller.rb
@@ -6,11 +6,11 @@ class DeskController < ApplicationController
 
   def cases
     data = Desk.retrive_all_cases
-    render json: data, status: 200
+    render json: data[:info], status: data[:status]
   end
 
   def retrive_labels
     data = Desk.retrive_all_labels
-    render json: data, status: 200
+    render json: data[:info], status: data[:status]
   end
 end
