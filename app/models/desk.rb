2684cc39ecabb5ea295135c6f6885c659092897b
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2014-02-19 21:18:19 -0800

    Added some error handling to model

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -8,20 +8,35 @@ class Desk
     _url     =  "https://pmcompany.desk.com/api/v2/cases"
     response = token_handler_obj _url
 
-    response.entries.map do |ech_case|
-      if ech_case.include?("_embedded")
-        return ech_case[1]['entries']
+    if response.present?
+      _tmp = ""
+      response.entries.map do |ech_case|
+        if ech_case.include?("_embedded")
+           _tmp = ech_case[1]['entries']
+        end
       end
+      data = {info: _tmp, status: 200}
+    else
+      data = {info: {error: "Could not retrive case info"}, status: 422}
     end
+
+    data
   end
 
   def self.retrive_all_labels
     _url     =  "https://pmcompany.desk.com/api/v2/labels"
     response = token_handler_obj _url
 
-    response['_embedded']['entries'].map do |ech_label|
-      {name: ech_label['name'], description: ech_label['description']}
+    if response.present?
+      _tmp = response['_embedded']['entries'].map do |ech_label|
+        {name: ech_label['name'], description: ech_label['description']}
+      end
+      data = {info: _tmp, status: 200}
+    else
+      data = {info: {error: "Could not retrive labels info"}, status: 422}
     end
+
+    data
   end
 
 end
